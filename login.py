f38b5240913f8b6e36486c628165780c0f081e6e
rubenAlbuquerque <45262080+rubenAlbuquerque@users.noreply.github.com>
2020-04-12 12:28:20 +0100

    Learn how to use css file in PyQt5

--- login.py
+++ login.py
@@ -1,103 +1,61 @@
 
-from PyQt5 import QtWidgets
 import sys
-from PyQt5 import QtGui, QtCore
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
-class MainWindow(QtWidgets.QMainWindow):
+
+class MainWindow():
+
     def __init__(self):
-        super(MainWindow, self).__init__()
-        self.init_window()
-    
-    def init_window(self):
-        
-        self.title = QtWidgets.QLabel(self)
-        self.title.setText("Login")
-        #self.title.setFont()
+        super().__init__()
+        self.app = QApplication(sys.argv)
+        self.window = QMainWindow()
 
-        self.Line_Email = QtWidgets.QLineEdit(self)
-        #self.Line_Email.setFixedWidth(470)
+        with open('style.qss') as f:
+            self.style = f.read()
 
-        self.Line_Pass = QtWidgets.QLineEdit(self)
-        #self.Line_Pass.setFixedWidth(470)
+        self.initUI()
+        self.window.setWindowTitle(" ")
+        self.window.setWindowIcon(QIcon('Capturar.PNG'))
+        self.window.setGeometry(100, 100, 1700, 850)
 
-        # Quit button
-        self.qbtn = QtWidgets.QPushButton("Login", self)
-        self.qbtn.setToolTip('Click to exit the program...')
-        #self.qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        # self.qbtn.resize(self.qbtn.sizeHint())
-
-
-
-        # Container widget
-        self.main_widget = QtWidgets.QWidget(self)
-
-        ## Set layout
-        # Containers
-        self.main_layout = QtWidgets.QVBoxLayout(self.main_widget)
-        #self.main_layout.sizeConstraint = QtWidgets.QLayout.SetDefaultConstraint
-
-        self.main_widget.setLayout(self.main_layout)
-        self.setCentralWidget(self.main_widget)
-        
-        
-        self.Box1 = QtWidgets.QHBoxLayout()
-        self.Box1.addWidget(self.title)
-
-        self.Box2 = QtWidgets.QHBoxLayout()
-        self.Box2.addWidget(self.Line_Email)
-
-        self.Box3 = QtWidgets.QHBoxLayout()
-        self.Box3.addWidget(self.Line_Pass)
-
-        self.Box4 = QtWidgets.QHBoxLayout()
-        self.Box4.addWidget(self.qbtn)
-        
-        self.main_layout.addLayout(self.Box1)
-        self.main_layout.addStretch(1)
-        self.main_layout.addLayout(self.Box1)
-        self.main_layout.addLayout(self.Box1)
-        self.main_layout.addStretch(1)
-        self.main_layout.addLayout(self.Box1)
+        self.window.show()
+        self.app.setStyleSheet(self.style)
+        sys.exit(self.app.exec_())
 
 
 
 
+    def initUI(self):
+        with open('style.qss') as f:
+            stylesheet = f.read()
+
+        # self.setFixedSize(240, 480)
 
 
+        self.okButton = QPushButton("OK", self.window)
+        self.okButton.move(10, 100)
+        self.cancelButton = QPushButton("Cancel", self.window)
 
+
+        """"
+        hbox.addWidget(cancelButton)
+
+        hbox.addWidget(cancelButton)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
         """
-        layout = QtWidgets.QGridLayout()
-
-
-        # Label
-        label = QtWidgets.QLabel("label")
-        #label.setAlignment(QtCore.Qt.AlignHCenter)
-
-        button_login = QtWidgets.QPushButton("Login")
-
-
-        V_box1 = QtWidgets.QVBoxLayout()
-        V_box1.addWidget(label)
-        V_box1.addWidget(button_login)
-
-        self.setLayout(V_box1)
-        """
-
-
-
-        # MainWindow proprieties
-        self.setGeometry(100, 100, 1700, 850)
-        self.setWindowTitle(" ")
-        self.setStyleSheet("QMainWindow {background-color:rgb(40, 56, 94)}")
-        self.setWindowIcon(QtGui.QIcon('Capturar.PNG'))
-        self.show()
-        
-
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
-    sys.exit(app.exec_())
+
+    ex = MainWindow()
 
 
 
